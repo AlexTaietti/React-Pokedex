84560c2486e23086414fdc4042f980a55b463582
AlexTaietti <taiettialex@yahoo.it>
2021-01-09 19:07:51 +0000

    minor change to css naming

--- src/components/PokemonCard.js
+++ src/components/PokemonCard.js
@@ -11,8 +11,8 @@ const PokemonCard = ({ pokemon }) => {
         <div className="pokemon-card">
           <Link to={`/pokemon/${pokemon.name}`}>
           <figure aria-label={`${pokemon.name}'s sprite`} className="sprite">
-            <img alt={`${pokemon.name}'s back side`} className="sprite__side sprite__side--back" src={pokemon.sprite.back}/>
-            <img alt={`${pokemon.name}'s front side`} className="sprite__side sprite__side--front" src={pokemon.sprite.front}/>
+            <img alt={`${pokemon.name}'s back side`} className="sprite-side sprite-side--back" src={pokemon.sprite.back}/>
+            <img alt={`${pokemon.name}'s front side`} className="sprite-side sprite-side--front" src={pokemon.sprite.front}/>
             <figcaption>{pokemon.name}</figcaption>
           </figure>
           </Link>

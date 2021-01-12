9afc669fa84f02f474368c79fd203c0986ffd1c1
AlexTaietti <taiettialex@yahoo.it>
2021-01-12 22:06:00 +0000

    added a fade in effect to the card list

--- src/components/PokemonCard.js
+++ src/components/PokemonCard.js
@@ -1,13 +1,21 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { Link } from 'react-router-dom';
 
 import '../styles/PokemonCard.scss';
 
 const PokemonCard = ({ pokemon }) => {
 
+  const [visible, setVisibility] = useState(false);
+
+  useEffect(() => {
+
+    setVisibility(true);
+
+  }, []);
+
   return (
 
-      <li aria-label={pokemon.name} className="pokemon-card">
+      <li aria-label={pokemon.name} className={ visible ? "pokemon-card" : "pokemon-card hidden" }>
         <Link to={`/pokemon/${pokemon.name}`}>
         <figure aria-label={`${pokemon.name}'s sprite`} className="sprite">
           <img alt={`${pokemon.name}'s back side`} className="sprite-side sprite-side--back" src={pokemon.sprite.back}/>

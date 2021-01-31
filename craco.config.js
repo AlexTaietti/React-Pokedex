1e2f906b720ffad93d0153b5e805107350a89052
AlexTaietti <taiettialex@yahoo.it>
2021-01-31 15:40:09 +0000

    added a test folder

--- craco.config.js
+++ craco.config.js
@@ -2,19 +2,18 @@ const path = require("path");
 
 module.exports = {
 
-    webpack: {
-
-        alias: {
-            "@classes": path.resolve(__dirname, "src/classes/"),
-            "@components": path.resolve(__dirname, "src/components/"),
-            "@fonts": path.resolve(__dirname, "src/fonts/"),
-            "@hooks": path.resolve(__dirname, "src/state/hooks"),
-            "@images": path.resolve(__dirname, "src/images/"),
-            "@reducers": path.resolve(__dirname, "src/state/reducers/"),
-            "@styles": path.resolve(__dirname, "src/styles/"),
-            "@utils": path.resolve(__dirname, "src/utils/")
-        }
-
-    }
+  webpack: {
+      alias: {
+        "@classes"    : path.resolve(__dirname, "src/classes/"),
+        "@components" : path.resolve(__dirname, "src/components/"),
+        "@fonts"      : path.resolve(__dirname, "src/fonts/"),
+        "@hooks"      : path.resolve(__dirname, "src/state/hooks"),
+        "@images"     : path.resolve(__dirname, "src/images/"),
+        "@reducers"   : path.resolve(__dirname, "src/state/reducers/"),
+        "@styles"     : path.resolve(__dirname, "src/styles/"),
+        "@utils"      : path.resolve(__dirname, "src/utils/"),
+        "@tests"      : path.resolve(__dirname, "src/tests/")
+      }
+  }
 
 };

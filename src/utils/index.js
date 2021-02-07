4abf31dc2718221e7a29882e1028f64ffff3ad7d
AlexTaietti <taiettialex@yahoo.it>
2021-02-07 13:08:36 +0000

    [improvement] separated utils in different files in order to improve the imports structure and project wide readability

---
+++ src/utils/index.js
@@ -0,0 +1,4 @@
+import { fetchData, localStorageReducer, sessionStorageReducer } from './general';
+import { fetchPokemonByName, fetchPokemonDetails } from './pokemon';
+
+export { fetchData, fetchPokemonByName, fetchPokemonDetails, localStorageReducer, sessionStorageReducer };
